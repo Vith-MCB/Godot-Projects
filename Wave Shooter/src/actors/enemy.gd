a3c957cc3b709af2b014279c46d636cdc8491d32
Vitor Ribeiro <vitorriblacerda@gmail.com>
2022-08-10 20:20:37 -0300

    Enemy HP system

--- Wave Shooter/src/actors/enemy.gd
+++ Wave Shooter/src/actors/enemy.gd
@@ -5,11 +5,18 @@ var moviment = Vector2.ZERO
 
 var gotHit = false
 var recoil = 5
+var enemyHP = 3
 
 func _process(delta):
 	if Global.player != null and gotHit == false:
 		moviment = global_position.direction_to(Global.player.global_position)
+	elif gotHit:
+		moviment = lerp(moviment, Vector2.ZERO, 0.3)
+	
 	global_position += moviment * delta * speed
+	
+	if enemyHP <= 0:
+		queue_free()
 
 
 func _on_hitbox_area_entered(area):
@@ -18,6 +25,7 @@ func _on_hitbox_area_entered(area):
 		area.get_parent().queue_free()
 		gotHit = true
 		moviment = -moviment * recoil
+		enemyHP -= 1
 		$colorRestore.start()
 		
 
